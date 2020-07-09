adb188186aff3e851f12743d3e97b8b927ce34cf
Stuart Mesham <stumes3@gmail.com>
2020-07-09 17:43:05 +0200

    added bash script to fetch all data

---
+++ scripts/fetch_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/scripts
+
+echo 'fetching nchlt data'
+mkdir -p data/nchlt
+python3 scripts/get_nchlt.py --output_dir=data/nchlt
+
+echo
+
+echo 'fetching autshumato data'
+mkdir -p data/autshumato
+python3 scripts/get_autshumato.py --output_dir=data/autshumato
+
+echo
+
+echo 'fetching isolezwe data'
+mkdir -p data/isolezwe
+python3 scripts/get_isolezwe.py --output_dir=data/isolezwe
