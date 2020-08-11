3050cae79575ecc6e2d963e2d07ae0c515af54b7
Stuart Mesham <stumes3@gmail.com>
2020-08-11 21:23:58 +0200

    removed TODO from train_tokenizer.py

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -14,8 +14,6 @@ if not os.path.exists('tokenizers/isizulu'):
 tokenizer.save('tokenizers/isizulu')
 
 # Example usage
-tokenizer = CharBPETokenizer(merges_file='tokenizers/isizulu/merges.txt', vocab_file='tokenizers/isizulu/vocab.json')
+# tokenizer = CharBPETokenizer(merges_file='tokenizers/isizulu/merges.txt', vocab_file='tokenizers/isizulu/vocab.json')
 encoded = tokenizer.encode("Molweni ndisaphila nkosi.")
 print(encoded.tokens)
-
-# TODO: make it so that there isn't a word separator </w> before full stop token
