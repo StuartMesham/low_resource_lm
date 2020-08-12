4b5875fd1f6a27292b5993a4c1abf3571d9497f0
Stuart Mesham <stumes3@gmail.com>
2020-08-12 18:28:48 +0200

    disabled colab progress bars in train_tokenizer.py

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 zulu_corpus = ['data/autshumato/isizulu.txt', 'data/nchlt/isizulu.txt']
 
 tokenizer = CharBPETokenizer()
-tokenizer.train(zulu_corpus, vocab_size=args.vocab_size)
+tokenizer.train(zulu_corpus, vocab_size=args.vocab_size, show_progress=False)
 
 if not os.path.exists('tokenizers/isizulu'):
     os.makedirs('tokenizers/isizulu')
