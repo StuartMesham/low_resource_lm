64f0540437457f057cd8e071ea315bc81bd42996
Stuart Mesham <stumes3@gmail.com>
2020-08-12 14:38:33 +0200

    made vocab_size adjustable in train_tokenizer.py

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -1,12 +1,17 @@
 import os
+import argparse
 from tokenizers import CharBPETokenizer
 
 # https://github.com/huggingface/tokenizers/tree/master/bindings/python
 
+parser = argparse.ArgumentParser(description='Train Byte-Pair Encoding Tokenizer.')
+parser.add_argument('--vocab_size', required=False, default=5000, type=int, help='directory where output files will be saved')
+args = parser.parse_args()
+
 zulu_corpus = ['data/autshumato/isizulu.txt', 'data/nchlt/isizulu.txt']
 
 tokenizer = CharBPETokenizer()
-tokenizer.train(zulu_corpus)
+tokenizer.train(zulu_corpus, vocab_size=args.vocab_size)
 
 if not os.path.exists('tokenizers/isizulu'):
     os.makedirs('tokenizers/isizulu')
