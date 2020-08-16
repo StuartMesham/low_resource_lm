6b376d819ed7311227a0b3f1e969eff9684975a8
Luc Hayward <luchay10@gmail.com>
2020-08-16 21:23:34 +0200

    Fixed file locations

--- scripts/partition_datasets.py
+++ scripts/partition_datasets.py
@@ -13,26 +13,26 @@ parser.add_argument('--awd_lstm', default=False, help='Prepends a space char to 
 args = parser.parse_args()
 assert (args.test_split + args.train_split + args.valid_split == 100), "Dataset splits must add to 100"
 
-isolezwe_files = ['isizulu/isizulu.txt']
-autshumato_files = ['isizulu/isizulu.txt', 'sepedi/sepedi.txt']
-nchlt_files = ['isizulu/isizulu.txt', 'sepedi/sepedi.txt']
+isolezwe_files = ['isizulu.txt']
+autshumato_files = ['isizulu.txt', 'sepedi.txt']
+nchlt_files = ['isizulu.txt', 'sepedi.txt']
 datasets = [[args.autshumato_dir, autshumato_files],
             [args.isolezwe_dir, isolezwe_files],
             [args.nchlt_dir, nchlt_files]]
 
 for dataset in datasets:
     for file in dataset[1]:
-        with open(dataset[0] + file, 'r', encoding='utf-8') as inf:
+        with open(dataset[0] + file[:-4] + '/' + file, 'r', encoding='utf-8') as inf:
             corpus = inf.read()
             if args.awd_lstm:
                 corpus = ' ' + corpus.replace('\n', '\n ')
 
-            with open(dataset[0] + file[:-4] + '_test.txt', 'w', encoding='utf-8') as f:
+            with open(dataset[0] + file[:-4] + '/test.txt', 'w', encoding='utf-8') as f:
                 f.write(corpus[:int(len(corpus) * args.test_split / 100)])
 
-            with open(dataset[0] + file[:-4] + '_train.txt', 'w', encoding='utf-8') as f:
+            with open(dataset[0] + file[:-4] + '/train.txt', 'w', encoding='utf-8') as f:
                 f.write(corpus[int(len(corpus) * args.test_split / 100):int(len(corpus) * args.train_split / 100) + int(
                     len(corpus) * args.test_split / 100)])
 
-            with open(dataset[0] + file[:-4] + '_valid.txt', 'w', encoding='utf-8') as f:
+            with open(dataset[0] + file[:-4] + '/valid.txt', 'w', encoding='utf-8') as f:
                 f.write(corpus[int(len(corpus) * args.train_split / 100) + int(len(corpus) * args.test_split / 100):])
