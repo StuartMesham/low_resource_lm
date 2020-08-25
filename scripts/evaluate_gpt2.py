2ba3ef05f2c75c02dc2120d8e37f820e27e46325
Stuart Mesham <stumes3@gmail.com>
2020-08-25 12:20:57 +0200

    added strides to evaluate_gpt2.py

--- scripts/evaluate_gpt2.py
+++ scripts/evaluate_gpt2.py
@@ -1,40 +1,43 @@
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
-import math
+# https://huggingface.co/transformers/perplexity.html
+import os
 import argparse
+import torch
+from tqdm import tqdm
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 parser = argparse.ArgumentParser(description='Evaluate a trained GPT-2 model on a test set.')
 parser.add_argument('--model_dir', required=True, help='directory where model files are saved')
 parser.add_argument('--test_set', required=True, help='file containing test set')
 parser.add_argument('--input_block_size', default=32, type=int, required=False, help='size of each input example')
+parser.add_argument('--stride', default=512, type=int, required=False, help='size of each input example')
 args = parser.parse_args()
 
-
-tokenizer = GPT2TokenizerFast.from_pretrained(args.model_dir)
 model = GPT2LMHeadModel.from_pretrained(args.model_dir)
+tokenizer = GPT2TokenizerFast.from_pretrained(args.model_dir)
 
-validation_dataset = TextDataset(
-    tokenizer=tokenizer,
-    file_path=args.test_set,
-    block_size=args.input_block_size,
-)
+f = open(args.test_set, 'r')
+test_set = f.read()
+f.close()
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer,
-    mlm=False
-)
+encodings = tokenizer(test_set, return_tensors='pt')
 
-training_args = TrainingArguments(
-    output_dir=args.model_dir,
-)
+lls = []
+for i in tqdm(range(1, encodings.input_ids.size(1), args.stride)):
+    begin_loc = max(i + args.stride - args.input_block_size, 0)
+    end_loc = i + args.stride
+    input_ids = encodings.input_ids[:,begin_loc:end_loc]
+    target_ids = input_ids.clone()
+    target_ids[:,:-args.stride] = -100
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    eval_dataset=validation_dataset,
-    prediction_loss_only=True,
-)
+    with torch.no_grad():
+        outputs = model(input_ids, labels=target_ids)
+        log_likelihood = outputs[0] * args.stride
 
-eval_loss = trainer.evaluate()['eval_loss']
-print('eval_loss:', eval_loss)
-print('perplexity:', math.exp(eval_loss))
+    lls.append(log_likelihood)
+
+ppl = torch.exp(torch.stack(lls).sum() / encodings.input_ids.size(1)).item()
+bpc = torch.pow(2, torch.stack(lls).sum() / len(test_set)).item()
+print('ppl:', ppl)
+print('bpc:', bpc)
