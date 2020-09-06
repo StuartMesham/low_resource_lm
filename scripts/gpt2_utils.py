1ff8a2de25d396f91edf26667452db27ed526dc3
Stuart Mesham <stumes3@gmail.com>
2020-09-06 21:37:21 +0200

    put tokenizer and dataset caches in the same directory

--- scripts/gpt2_utils.py
+++ scripts/gpt2_utils.py
@@ -1,16 +1,98 @@
 import hashlib
 import json
 import os
+import logging
+import time
+import pickle
+from typing import List, Tuple
+from filelock import FileLock
 import torch
+from torch.utils.data.dataset import Dataset
 from tokenizers import ByteLevelBPETokenizer
 from tqdm.auto import tqdm
 from transformers import GPT2TokenizerFast, GPT2Config, GPT2LMHeadModel, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from multilingual_data_collator import MultilingualDataCollatorForLanguageModeling
-from multilingual_dataset import MultilingualTextDataset
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-TOKENIZER_CACHE_DIR = 'cached_tokenizers'
+CACHE_DIR = 'caches'
+
+logger = logging.getLogger(__name__)
+
+# logger.setLevel(logging.WARNING)
+
+
+class MultilingualTextDataset(Dataset):
+    def __init__(
+            self,
+            tokenizer: GPT2TokenizerFast,
+            dataset_files: List[Tuple[int, str]],
+            block_size: int,
+            use_token_type_ids=True,
+    ):
+
+        self.examples = []
+        for language_id, file_path in dataset_files:
+            assert os.path.isfile(file_path)
+
+            block_size = block_size - tokenizer.num_special_tokens_to_add(pair=False)
+
+            m = hashlib.md5()
+            m.update(tokenizer.cache_id.encode())
+            m.update(str(block_size).encode())
+            m.update(file_path.encode())
+            cache_id = m.hexdigest()
+
+            cached_features_file = os.path.join(
+                CACHE_DIR, 'dataset_{}'.format(cache_id),
+            )
+
+            # Make sure only the first process in distributed training processes the dataset,
+            # and the others will use the cache.
+            lock_path = cached_features_file + ".lock"
+            with FileLock(lock_path):
+                language_examples = []
+
+                if os.path.exists(cached_features_file):
+                    start = time.time()
+                    with open(cached_features_file, "rb") as handle:
+                        language_examples = pickle.load(handle)
+                    logger.info(
+                        f"Loading features from cached file {cached_features_file} [took %.3f s]", time.time() - start
+                    )
+
+                else:
+                    logger.info(f"Creating features from dataset file {file_path}")
+
+                    with open(file_path, encoding="utf-8") as f:
+                        text = f.read()
+
+                    tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
+
+                    for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
+                        tokens = tokenizer.build_inputs_with_special_tokens(tokenized_text[i : i + block_size])
+                        language_examples.append(tokens)
+                    # Note that we are losing the last truncated example here for the sake of simplicity (no padding)
+                    # If your dataset is small, first you should look for a bigger one :-) and second you
+                    # can change this behavior by adding (model specific) padding.
+
+                    start = time.time()
+                    with open(cached_features_file, "wb") as handle:
+                        pickle.dump(language_examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                    logger.info(
+                        "Saving features into cached file %s [took %.3f s]", cached_features_file, time.time() - start
+                    )
+                if use_token_type_ids:
+                    # add language IDs
+                    self.examples += [[tokens, [language_id]*len(tokens)] for tokens in language_examples]
+                else:
+                    self.examples += tokens
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i) -> torch.Tensor:
+        return torch.tensor(self.examples[i], dtype=torch.long)
 
 
 def get_tokenizer(train_data, vocab_size):
@@ -20,7 +102,7 @@ def get_tokenizer(train_data, vocab_size):
         m.update(file.encode())
     cache_id = m.hexdigest()
 
-    cached_tokenizer_file = os.path.join(TOKENIZER_CACHE_DIR, cache_id)
+    cached_tokenizer_file = os.path.join(CACHE_DIR, 'tokenizer_{}'.format(cache_id))
 
     if not os.path.exists(cached_tokenizer_file):
         os.makedirs(cached_tokenizer_file)
